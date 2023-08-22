50c2ae7b962e4c308f5a45cfd5eb00ea0347031d
Ajit <asingh@appriss.com>
2023-08-22 10:40:02 -0400

    remove unwanted line,

--- src/test/java/com/ajitechweb/springbootexample/ServiceArrayTest.java
+++ src/test/java/com/ajitechweb/springbootexample/ServiceArrayTest.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import static com.ajitechweb.springbootexample.ServiceArray.randomaArrayGnerator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -15,6 +17,43 @@ public class ServiceArrayTest {
 
     @Autowired
     private ServiceArray numberService;
+
+    @Test
+    public  void testThreeArrayGenerator(){
+
+        List<Integer> array1  = randomaArrayGnerator(15,50);
+
+        List<Integer> array2  = randomaArrayGnerator(15,50);
+
+        List<Integer> array3  = randomaArrayGnerator(15,50);
+
+        assertEquals(array1.size(),15);
+        assertEquals(array2.size(),15);
+        assertEquals(array3.size(),15);
+
+
+    }
+
+    @Test
+    public void testRandomaArrayGnerator(){
+
+        List randomNumberList = numberService.randomaArrayGnerator(15,50);
+        assertTrue(randomNumberList.size()!=0);
+        assertTrue(randomNumberList!=null);
+    }
+
+    @Test
+    public void testArrayOfAvailableNumbers(){
+        HashSet uniqueSet= new HashSet();
+        uniqueSet.add(2);
+        uniqueSet.add(10);
+        uniqueSet.add(15);
+        uniqueSet.add(17);
+
+        HashSet availableNummbers = numberService.arrayOfAvailableNumbers(uniqueSet);
+        assertTrue(availableNummbers.size()!=0);
+        assertTrue(availableNummbers!=null);
+    }
 
     @Test
     public void testCheckPrime() {
